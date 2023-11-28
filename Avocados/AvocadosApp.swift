7a0b2638bd3c32200b1453c91b73de486d3e195a
Kasthuri <tkasthu.90@gmail.com>
2023-11-29 10:49:01 +1100

    Initial commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Kasthuri Tandavarayane on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
